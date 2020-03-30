efa2659148f4fc986f313862b53f4d19e1784fb7
NirbhayVashisht <nirbhayvashisht@gmail.com>
2020-03-30 16:10:50 +0530

    Q28 added

---
+++ src/FundamentalsAndPatterns/Q28.java
@@ -0,0 +1,34 @@
+
+/*      Pythagoras Triplet
+If m>n>0 are integers, then (m2-n2, 2mn, m2-n2) is a Pythagorean triple.
+ */
+package FundamentalsAndPatterns;
+
+import java.util.Scanner;
+
+public class Q28 {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        long n = scn.nextLong();
+        evaluate(n);
+    }
+    static void evaluate(long n) {
+        if (n == 1 || n == 2)
+            System.out.println(-1);
+
+        else if (n % 2 == 0) {
+
+            // Calculating for even case
+            System.out.println(((n * n) / 4) - 1 + " " + (((n * n) / 4) + 1));
+        }
+
+        else if (n % 2 != 0) {
+
+            System.out.println((n * n - 1) / 2 + " " + (n * n + 1) / 2);
+        }
+    }
+
+}
+}
